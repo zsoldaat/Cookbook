28274a417bdf7f96cf33cd56a5543fac0101d148
zsoldaat <zac.soldaat@gmail.com>
2024-12-20 16:34:49 -0500

    facilitate entering ingredient data

---
+++ Cookbook/AddIngredientModal.swift
@@ -0,0 +1,18 @@
+//
+//  AddIngredientModal.swift
+//  Cookbook
+//
+//  Created by Zac Soldaat on 2024-12-18.
+//
+
+import SwiftUI
+
+struct AddIngredientModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddIngredientModal()
+}
