db122c9066af2909d22638f5db58cf0998d76bc0
zsoldaat <zac.soldaat@gmail.com>
2025-01-09 10:54:54 -0500

    Nice initial card view

---
+++ Cookbook/UI Elements/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Cookbook
+//
+//  Created by Zac Soldaat on 2025-01-09.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
