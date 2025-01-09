10b48899a339f091f421f40f403dc3291e75337a
zsoldaat <zac.soldaat@gmail.com>
2025-01-09 13:13:41 -0500

    change recipe cell

--- Cookbook/Cells/RecipeCell.swift
+++ Cookbook/Cells/RecipeCell.swift
@@ -11,11 +11,28 @@ struct RecipeCell: View {
     
     let recipe: Recipe
     
+    let imageSize: CGFloat = 75
+    
     var body: some View {
-        Text(recipe.name)
+        
+        HStack(alignment: .center) {
+            AsyncImage(url: recipe.imageUrl) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: imageSize, height: imageSize, alignment: .center)
+                    .clipped()
+            } placeholder: {
+                Color.gray.frame(width: imageSize, height: imageSize)
+            }
+            VStack(alignment: .leading) {
+                Text(recipe.name).font(.headline)
+                Text(recipe.instructions).lineLimit(2).font(.subheadline).foregroundStyle(.secondary, .secondary)
+            }
+            
+        }
     }
 }
 
-#Preview {
-    RecipeCell(recipe: Recipe(name: "Hello", instructions: "", ingredients: []))
-}
+//#Preview {
+//    RecipeCell(recipe: Recipe(name: "Hello", instructions: "Instructions", ingredients: [Ingredient(name: "Tomato", quantityWhole: 1, quantityFraction: 0.5, unit: "item")]))
+//}
