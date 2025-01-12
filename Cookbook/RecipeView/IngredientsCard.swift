023617f7bbf122038461a57a680168b30e7fe1a5
zsoldaat <zac.soldaat@gmail.com>
2025-01-12 12:22:51 -0500

    refactor recipe view because xcode was yelling

---
+++ Cookbook/RecipeView/IngredientsCard.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientsCard.swift
+//  Cookbook
+//
+//  Created by Zac Soldaat on 2025-01-12.
+//
+
+import SwiftUI
+
+struct IngredientsCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientsCard()
+}
