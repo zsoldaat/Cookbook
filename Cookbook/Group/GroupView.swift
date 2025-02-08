5a1db996cf0ea8019a0c8e62e689907329195f4b
zsoldaat <zac.soldaat@gmail.com>
2025-02-08 16:06:11 -0500

    add ability to remove recipes from groups

--- Cookbook/Group/GroupView.swift
+++ Cookbook/Group/GroupView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct GroupView: View {
     
+    @Environment(\.modelContext) var context
+    
     let group: RecipeGroup
     
     var body: some View {
         
         List {
             ForEach(group.recipes!) {recipe in
-                Text(recipe.name)
+                RecipeCell(recipe: recipe)
+            }
+            .onDelete { indexSet in
+                for i in indexSet {
+                    group.removeRecipe(recipe: group.recipes![i])
+                }
+                try! context.save()
             }
         }
         .navigationTitle(group.name)
--- Cookbook/Model/RecipeGroup.swift
+++ Cookbook/Model/RecipeGroup.swift
@@ -21,9 +21,21 @@ class RecipeGroup: Identifiable, Hashable, ObservableObject {
     }
     
     func addRecipe(recipe: Recipe) {
+        
+        if (recipes!.map{$0.id}.contains(recipe.id)) {
+            print("This recipe has already been added")
+            return
+        }
+        
         self.recipes!.append(recipe)
         
         //do cloudkit stuff
+    }
+    
+    func removeRecipe(recipe: Recipe) {
+        recipes = recipes!.filter{$0.id != recipe.id}
+        
+        // do cloudkit stuff
     }
     
 }
