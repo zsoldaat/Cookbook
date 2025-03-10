b08f3ba4ed6a4db3374b97db2dc2837f63fe2a64
zsoldaat <zac.soldaat@gmail.com>
2025-03-10 16:30:28 -0400

    user chooses between link or manual entry

--- Cookbook/Recipe/CreateEditRecipeView.swift
+++ Cookbook/Recipe/CreateEditRecipeView.swift
@@ -18,6 +18,7 @@ struct CreateEditRecipeView: View {
     @State var ingredientToEdit: Ingredient?
     var isNewRecipe: Bool
     
+    @State private var linkFormShowing: Bool = true
     @State private var alertShowing: Bool = false
     
     var body: some View {
@@ -65,94 +66,223 @@ struct CreateEditRecipeView: View {
     
     
     var form: some View {
-        Form {
+        
+        VStack {
             
-            Section {
-                let linkBinding = Binding<String>(get: {
-                    recipe.link ?? ""
-                }, set: {
-                    recipe.link = $0
-                })
+            HStack {
+                Button {
+                    linkFormShowing = true
+                } label: {
+                    Label("From link", systemImage: "link")
+                }
+                .padding()
+                .foregroundStyle(.foreground)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(.accent, lineWidth: linkFormShowing ? 2 : 0)
+                    )
                 
-                TextField("", text: linkBinding)
-                    .submitLabel(.done)
-                    .onSubmit {
-                        Task {
-                            guard let link = recipe.link else {return}
-                            
-                            if let url = URL(string: link) {
-                                let scraper = Scraper(url: url)
-                                
-                                guard let recipeData = await scraper.getRecipeData() else {return}
-                                
-                                if let name = recipeData.name {
-                                    recipe.name = name
-                                }
-                                
-                                if let instructions = recipeData.instructions {
-                                    recipe.instructions = instructions
-                                }
-                                
-                                if let imageUrl = recipeData.imageUrls?.first {
-                                    recipe.imageUrl = URL(string: imageUrl)
-                                }
-                                
-                                if let ingredients = recipeData.ingredients {
-                                    recipe.ingredients = ingredients
+                Button {
+                    linkFormShowing = false
+                } label: {
+                    Label("Manual", systemImage: "list.bullet")
+                }
+                .padding()
+                .foregroundStyle(.foreground)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(.accent, lineWidth: !linkFormShowing ? 2 : 0)
+                    )
+            }
+            
+            if (linkFormShowing == true) {
+                Form {
+                    Section {
+                        let linkBinding = Binding<String>(get: {
+                            recipe.link ?? ""
+                        }, set: {
+                            recipe.link = $0
+                        })
+                        
+                        TextField("", text: linkBinding)
+                            .submitLabel(.done)
+                            .onSubmit {
+                                Task {
+                                    guard let link = recipe.link else {return}
+                                    
+                                    if let url = URL(string: link) {
+                                        let scraper = Scraper(url: url)
+                                        
+                                        guard let recipeData = await scraper.getRecipeData() else {return}
+                                        
+                                        if let name = recipeData.name {
+                                            recipe.name = name
+                                        }
+                                        
+                                        if let instructions = recipeData.instructions {
+                                            recipe.instructions = instructions
+                                        }
+                                        
+                                        if let imageUrl = recipeData.imageUrls?.first {
+                                            recipe.imageUrl = URL(string: imageUrl)
+                                        }
+                                        
+                                        if let ingredients = recipeData.ingredients {
+                                            recipe.ingredients = ingredients
+                                        }
+                                        
+                                        linkFormShowing = false
+                                    }
                                 }
                             }
-                        }
+                    } header: {
+                        Text("Link")
+                    }  footer: {
+                        Text("Cookbook will pull recipe data from the link provided")
                     }
-            } header: {
-                Text("Link")
-            }  footer: {
-                Text("Cookbook will pull recipe data from the link provided")
-            }
-            Section {
-                TextField("", text: $recipe.name)
-            } header: {
-                Text("Name")
-            }
-            
-            Section {
-                TextField("", text: $recipe.instructions, axis: .vertical)
-                    .lineLimit(5...)
-            } header: {
-                Text("Instructions")
-            }
-            
-            ListButton(text: "Add Ingredients", imageSystemName: "plus") {
-                ingredientToEdit = Ingredient(name: "", quantityWhole: 1, quantityFraction: 0, unit: .item, index: recipe.getNextIngredientIndex())
-            }
-            
-            if (!recipe.ingredients!.isEmpty) {
-                Section {
-                    List {
-                        ForEach(recipe.ingredients!.sorted {$0.index < $1.index}) {ingredient in
-                            IngredientCell(ingredient: ingredient)
-                                .swipeActions {
-                                    Button(role: .destructive) {
-                                        recipe.ingredients!.removeAll(where: {$0.id == ingredient.id})
-                                        
-                                    } label: {
-                                        Label("Delete", systemImage: "trash")
-                                            .labelStyle(.iconOnly)
-                                    }
-                                    .tint(.red)
-                                    
-                                    Button {
-                                        ingredientToEdit = ingredient
-                                    } label: {
-                                        Label("Edit", systemImage: "square.and.pencil")
-                                            .labelStyle(.iconOnly)
-                                    }
-                                    .tint(.yellow)
-                                }
-                        }
+                }
+            } else {
+                Form {
+                    Section {
+                        TextField("", text: $recipe.name)
+                    } header: {
+                        Text("Name")
                     }
                     
-                } header: {Text("Ingredients")}
+                    Section {
+                        TextField("", text: $recipe.instructions, axis: .vertical)
+                            .lineLimit(5...)
+                    } header: {
+                        Text("Instructions")
+                    }
+                    
+                    ListButton(text: "Add Ingredients", imageSystemName: "plus") {
+                        ingredientToEdit = Ingredient(name: "", quantityWhole: 1, quantityFraction: 0, unit: .item, index: recipe.getNextIngredientIndex())
+                    }
+                    
+                    if (!recipe.ingredients!.isEmpty) {
+                        Section {
+                            List {
+                                ForEach(recipe.ingredients!.sorted {$0.index < $1.index}) {ingredient in
+                                    IngredientCell(ingredient: ingredient)
+                                        .swipeActions {
+                                            Button(role: .destructive) {
+                                                recipe.ingredients!.removeAll(where: {$0.id == ingredient.id})
+                                                
+                                            } label: {
+                                                Label("Delete", systemImage: "trash")
+                                                    .labelStyle(.iconOnly)
+                                            }
+                                            .tint(.red)
+                                            
+                                            Button {
+                                                ingredientToEdit = ingredient
+                                            } label: {
+                                                Label("Edit", systemImage: "square.and.pencil")
+                                                    .labelStyle(.iconOnly)
+                                            }
+                                            .tint(.yellow)
+                                        }
+                                }
+                            }
+                            
+                        } header: {Text("Ingredients")}
+                    }
+                }
             }
         }
+        
+//        
+//        
+//        Form {
+//            Section {
+//                let linkBinding = Binding<String>(get: {
+//                    recipe.link ?? ""
+//                }, set: {
+//                    recipe.link = $0
+//                })
+//                
+//                TextField("", text: linkBinding)
+//                    .submitLabel(.done)
+//                    .onSubmit {
+//                        Task {
+//                            guard let link = recipe.link else {return}
+//                            
+//                            if let url = URL(string: link) {
+//                                let scraper = Scraper(url: url)
+//                                
+//                                guard let recipeData = await scraper.getRecipeData() else {return}
+//                                
+//                                if let name = recipeData.name {
+//                                    recipe.name = name
+//                                }
+//                                
+//                                if let instructions = recipeData.instructions {
+//                                    recipe.instructions = instructions
+//                                }
+//                                
+//                                if let imageUrl = recipeData.imageUrls?.first {
+//                                    recipe.imageUrl = URL(string: imageUrl)
+//                                }
+//                                
+//                                if let ingredients = recipeData.ingredients {
+//                                    recipe.ingredients = ingredients
+//                                }
+//                            }
+//                        }
+//                    }
+//            } header: {
+//                Text("Link")
+//            }  footer: {
+//                Text("Cookbook will pull recipe data from the link provided")
+//            }
+//            Section {
+//                TextField("", text: $recipe.name)
+//            } header: {
+//                Text("Name")
+//            }
+//            
+//            Section {
+//                TextField("", text: $recipe.instructions, axis: .vertical)
+//                    .lineLimit(5...)
+//            } header: {
+//                Text("Instructions")
+//            }
+//            
+//            ListButton(text: "Add Ingredients", imageSystemName: "plus") {
+//                ingredientToEdit = Ingredient(name: "", quantityWhole: 1, quantityFraction: 0, unit: .item, index: recipe.getNextIngredientIndex())
+//            }
+//            
+//            if (!recipe.ingredients!.isEmpty) {
+//                Section {
+//                    List {
+//                        ForEach(recipe.ingredients!.sorted {$0.index < $1.index}) {ingredient in
+//                            IngredientCell(ingredient: ingredient)
+//                                .swipeActions {
+//                                    Button(role: .destructive) {
+//                                        recipe.ingredients!.removeAll(where: {$0.id == ingredient.id})
+//                                        
+//                                    } label: {
+//                                        Label("Delete", systemImage: "trash")
+//                                            .labelStyle(.iconOnly)
+//                                    }
+//                                    .tint(.red)
+//                                    
+//                                    Button {
+//                                        ingredientToEdit = ingredient
+//                                    } label: {
+//                                        Label("Edit", systemImage: "square.and.pencil")
+//                                            .labelStyle(.iconOnly)
+//                                    }
+//                                    .tint(.yellow)
+//                                }
+//                        }
+//                    }
+//                    
+//                } header: {Text("Ingredients")}
+//            }
+//        }
     }
 }
