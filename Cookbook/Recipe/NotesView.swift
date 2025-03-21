79334319e544239ba38cb6eeddce023c0e3e9857
zsoldaat <zac.soldaat@gmail.com>
2025-03-21 12:50:15 -0400

    Add notes field to recipes

---
+++ Cookbook/Recipe/NotesView.swift
@@ -0,0 +1,18 @@
+//
+//  NotesView.swift
+//  Cookbook
+//
+//  Created by Zac Soldaat on 2025-03-21.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotesView()
+}
